bb6f403c78b91bbaf8bd2fa945f55e37abf53dcf
arzuakov@yahoo.com <arzuakov@yahoo.com>
2021-01-22 17:44:48 +0300

    w22.01.2021

--- src/components/btn-create-new-exp/BtnCreateNewExp.js
+++ src/components/btn-create-new-exp/BtnCreateNewExp.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const BtnCreateNewExp = () => (
-    <div class="main__item btn-create-new-exp">+</div>
+    <div className="main__item btn-create-new-exp">+</div>
 )
 
 export default BtnCreateNewExp;
